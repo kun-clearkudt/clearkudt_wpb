67bf7b228bbfd48b2dbf1654910cd636d1ef63c5
Kun Srithaporn <kun@clearkudt.com>
2012-05-25 17:05:23 +0700

    first time

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 define('DB_NAME', 'clearkudt_wpb');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'clearkudt_wpb');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin123');
+define('DB_PASSWORD', '63T3Al4Y');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'clearkudt_wpb');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
